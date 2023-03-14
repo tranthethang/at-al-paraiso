5f1f96f99fc56dbc19c972e71cc453be7f44c33f
tranthethang <tranthethang@gmail.com>
2023-03-14 18:03:28 +0700

    fill fields for migrate script

--- database/migrations/2023_03_14_023637_create_room_table.php
+++ database/migrations/2023_03_14_023637_create_room_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,11 @@ return new class extends Migration
     {
         Schema::create('room', function (Blueprint $table) {
             $table->id();
+            $table->string('room_name');
+            $table->text('description')->nullable();
+            $table->integer('hotel_id');
+            $table->integer('room_type_id');
+            $table->float('current_price');
             $table->timestamps();
         });
     }
